e6300e6cc811d1b7de2cef330d137e4c21ab3849
joeypark <joeypark84@gmail.com>
2016-01-05 00:15:59 +0900

    프로젝트 최초 커밋

---
+++ server/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secrets.json');
+$client->addScope(Google_Service_Oauth2::PLUS_LOGIN);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive_service = new Google_Service_Drive($client);
+  $files_list = $drive_service->files->listFiles(array())->getItems();
+  echo json_encode($files_list);
+}
+else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
